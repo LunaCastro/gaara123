b2c00b316c1838263f080f49b15d4500e8a1e615
LunaCastro <lunapereira921@gmail.com>
2023-02-13 12:11:29 -0300

    aulaaa

---
+++ Toggle.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+interface IToggle{
+  label: string 
+  options: Array<string>
+  value: string
+  onChange:(option: string) => void
+
+}
+export default function Toggle({ label, options, value, onChange }: IToggle) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.optionsContainer}>
+        {options.map((option) => (
+          <Button
+            color={option === value ? '#2ddada' : '#950ea1'}
+            onPress={() => onChange(option)}
+            title={option}
+            key={option}
+          />
+        ))}
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    paddingBottom: 12,
+  },
+  label: {
+    fontSize: 14,
+    padding: 4,
+  },
+  optionsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+})
