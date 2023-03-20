b749dda7809d2498ae190ec255515ab48da6f53d
LunaCastro <lunapereira921@gmail.com>
2023-03-20 15:42:02 -0300

    lunita

--- src/components/Marker/index.tsx
+++
@@ -1,8 +0,0 @@
-import {View} from 'react-native';
-import {styles} from './styles'
-
-export function Marker() {
-    return (
-        <View style={styles.ball} />
-    )
-}
--- src/styles/colors.ts
+++
@@ -1,9 +0,0 @@
-export const colors = {
-    primary: '#87CEFA',
-    secondary: '#FF1493',
-    third: '#66CDAA',
-    thirdLight: '#D9D9D9',
-    orange: '#FFA500',
-    black: '#000',
-
-}
